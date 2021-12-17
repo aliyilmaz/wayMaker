ac7f93b09b781eb58ff48bc8d4a7059f7dfcc47c
Ali Yılmaz <aliyilmaz.work@gmail.com>
2021-12-17 07:23:35 +0300

    Layer and parameter extraction.
    Layer and parameter extraction has been stabilized.

--- src/layerMaker.php
+++ src/layerMaker.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    layerMaker
- * @version    Release: 1.0.0
+ * @version    Release: 1.0.1
  * @license    GPL3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Layer and method parser.
@@ -29,16 +29,18 @@ class layerMaker
 
         // The layer and parameter are being parsed.
         if(strstr($str, ':')){ 
-            $assets = explode(':', $str); } else { $output['layer'] = $str; }
+            $assets = explode(':', trim($str, ':')); } else { $assets[] = $str; }
+
+        // If there is no layer parameter, only the layer is defined
+        if(count($assets) == 1){ $output['layer'] = $assets[0]; }
 
         // If there is a layer and its parameter, it is assigned to the variable.
-        if(count($assets) == 2){ 
-            list($output['layer'], $output['params']) = $assets; }
+        if(count($assets) == 2){ list($output['layer'], $output['params']) = $assets; }
 
-        // If the layer parameter is multiple, it is parsed.
+        // Parameters are obtained
         if(isset($output['params'])){
-            if(strstr($output['params'], '@')){
-                $output['params'] = explode('@', $output['params']); }}
+            $output['params'] = (strstr($output['params'], '@')) ? explode('@', trim($output['params'], '@')) : $output['params'] = [$output['params']];
+        }
         
         return $output;
     }
